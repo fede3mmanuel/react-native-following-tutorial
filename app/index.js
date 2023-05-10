5885c6559a8cd9e245acc5de15eaf357294c07ac
Emmanuel Sanchez <fede@fede.website>
2023-05-10 14:29:48 -0600

    setup

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import Layout from "./_layout";
+
+const Home = () => {
+    return (
+            <View>
+                <Text>Home</Text>
+            </View>
+        );
+}
+
+export default Home;
